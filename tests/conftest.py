74063b0d9c1997931a24e387e660a9459c439154
Manoel Lira <dilsonlira@gmail.com>
2020-11-23 17:43:00 -0300

    conftest.py adjustments

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,3 +2,5 @@ import sys
 
 sys.path.append('/easylog')
 sys.path.append('/home/runner/work/easylog')
+sys.path.append('/home/runner/work/easylog/easylog')
+sys.path.append('/home/runner/work/easylog/easylog/easylog')
