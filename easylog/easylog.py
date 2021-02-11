1d0e59e018d2e65c1f1b3702d95b24eb7d0677e7
Manoel Lira <dilsonlira@gmail.com>
2021-02-11 17:55:44 -0300

    Ability to handle multiple arguments in log()

--- easylog/easylog.py
+++ easylog/easylog.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python
 
-from helpers import get_frame_data, make_header, make_body
+from helpers import get_frame_data, make_header, make_body, print_log
 
 
-def log(expression):
-    """Prints name and current value of variable or expression"""
-
-    if expression is None:
-        return
+def log(*variables):
+    """Prints name and value of variables passed"""
 
     frame_data = get_frame_data()
 
@@ -21,9 +18,23 @@ def log(expression):
 
     log_header = make_header(frame_data)
 
-    log_body = make_body(frame_data, expression)
+    if variables:
+        variable_names = frame_data['arguments_list']
+        variable_values = variables
 
-    print(f'[{log_header}] {log_body}')
+        for name, value in zip(variable_names, variable_values):
+            log_body = make_body(name, value)
+            print_log(log_header, log_body)
+    else:
+        for name, value in frame_data['caller_vars'].items():
+
+            log_body = make_body(name, value)
+
+            is_dunder = name.startswith('__') and name.endswith('__')
+            is_function = hasattr(value, '__call__')
+
+            if not is_dunder and not is_function:
+                print_log(log_header, log_body)
 
 
 if __name__ == '__main__':
--- easylog/helpers.py
+++ easylog/helpers.py
@@ -2,13 +2,19 @@ from inspect import currentframe, getouterframes
 
 
 def get_frame_data():
-    """Get expression passed as argument and other necessary info"""
+    """Returns a dictionary with
+        - a list of variables in the scope where log was called
+        - a list of arguments passed in log function
+        - the file name where log was called
+        - the line number where log was called
+        - the name of the function where log was called
+    """
 
     try:
         current_frame = currentframe()
-        function_name = getouterframes(current_frame)[1][3]
+        log_function_name = getouterframes(current_frame)[1][3]
         outer_frame = getouterframes(current_frame)[2]
-        caller, path, line_number, scope, code = outer_frame[:5]
+        caller, path, line_number, function_name, code = outer_frame[:5]
 
         if path == '<stdin>':
             # Invocation in a Python interactive shell
@@ -20,49 +26,55 @@ def get_frame_data():
 
     caller_vars = caller.f_locals
     command = code[0].strip()
-    expression_passed = command[len(function_name) + 1:-1].strip()
+    arguments_string = command[len(log_function_name) + 1:-1].strip()
+    arguments_list = [item.strip() for item in arguments_string.split(',')]
     file_name = path.split('/')[-1]
 
     data = {
         'caller_vars': caller_vars,
-        'expression_passed': expression_passed,
+        'arguments_list': arguments_list,
         'file_name': file_name,
         'line_number': line_number,
-        'scope': scope
+        'function_name': function_name
     }
 
     return data
 
 
 def make_header(frame_data):
-    """Make log header"""
+    """Makes log header"""
 
     file_name = frame_data['file_name']
     line_number = frame_data['line_number']
-    scope = frame_data['scope']
+    function_name = frame_data['function_name']
 
-    header = f'{file_name} (line {line_number}) in {scope}'
+    header = f'{file_name} (line {line_number}) in {function_name}'
 
     return header
 
 
-def make_body(frame_data, evaluation):
-    """Make log body"""
+def make_body(name, value):
+    """Makes log body"""
 
-    expression = frame_data['expression_passed']
+    _type = type(value).__name__
 
-    expression_type = type(evaluation).__name__
+    if _type == 'str':
+        value = f'\'{value}\''
 
-    if expression_type == 'str':
-        evaluation = f'\'{evaluation}\''
-
-    log_body = f'({expression_type}) {expression} = {evaluation}'
+    log_body = f'({_type}) {name} = {value}'
 
     for char in ['\'', '\"']:
-        if expression.startswith(char) and expression.endswith(char):
+        if name.startswith(char) and name.endswith(char):
             # expression is a string literal
 
-            log_body = expression
+            log_body = name
             break
 
     return log_body
+
+
+def print_log(header, body):
+    '''Makes and prints log_line'''
+
+    log_line = f'[{header}] {body}'
+    print(log_line)
