c70ec32f0d51f7b98e83a56664f85bd04887c59d
Manoel Lira <dilsonlira@gmail.com>
2021-02-28 04:15:51 -0300

    Package files added

---
+++ easylog/__init__.py
@@ -0,0 +1,4 @@
+from easylog.easylog import log
+
+VERSION = '1.0.3'
+__version__ = VERSION
---
+++ setup.py
@@ -0,0 +1,38 @@
+import pathlib
+from setuptools import find_packages, setup
+
+from easylog import VERSION
+
+HERE = pathlib.Path(__file__).parent
+PACKAGE_NAME = 'easylog'
+AUTHOR = 'Manoel Lira'
+AUTHOR_EMAIL = 'dilsonlira@gmail.com'
+URL = 'https://github.com/dilsonlira/easylog'
+LICENSE = 'MIT'
+DESCRIPTION = 'The easy way to inspect variables in Python'
+LONG_DESCRIPTION = (HERE / 'README.md').read_text()
+LONG_DESC_TYPE = 'text/markdown'
+CLASSIFIERS = [
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+]
+REQUIRES_PYTHON = '>=3.6.0'
+
+setup(
+    name=PACKAGE_NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type=LONG_DESC_TYPE,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    url=URL,
+    license=LICENSE,
+    classifiers=CLASSIFIERS,
+    python_requires=REQUIRES_PYTHON,
+    packages=find_packages()
+    )
